fe0b1c47f253c51225761046fec40f92622d0893
2San <apexsun@narod.ru>
2023-02-26 23:57:31 +0300

    index UI save state

--- javascript/test.js
+++ javascript/test.js
@@ -19,6 +19,7 @@ var selMenu = (id) => {
     });
     $ch("m_" + id, 1);
     $sd(id, 1);
+    saveState();
 }
 
 var selFullViewCP = () => {
@@ -32,19 +33,25 @@ var selFullViewCP = () => {
         on: $('lcd_text_color').value,
         // pix: $('px_size').value,
         // brk: $('break_size').value
-    });//blue panel
+    });
     for (var i = 0; i < 16; i++) {
         for (var j = 0; j < 16; j++) {
             full_view_lcd.char(i, j, String.fromCharCode(i * 16 + j));
         }
     }
+    saveState();
 }
 
 var initFullViews = () => {
     var lcd0 = new CharLCD({ at: 'lcd0', rows: 1, cols: 1, rom: 'eu', off: '#fff', on: '#f00' });//1 custom symbol panel
     var lcd1 = new CharLCD({ at: 'lcd1', rows: 1, cols: 16, off: '#fff', on: '#000' });//left horizontal index 
     var lcdv = new CharLCD({ at: 'lcdv', rows: 16, cols: 1, off: '#fff', on: '#000' });//vertical index 
-    full_view_lcd = new CharLCD({ at: 'full_view_lcd', rows: 16, cols: 16, rom: 'eu', off: '#008', on: '#fe8' });//blue panel
+    full_view_lcd = new CharLCD({
+        at: 'full_view_lcd', rows: 16, cols: 16,
+        rom: $('full_view_cp').value,
+        off: $('lcd_bg_color').value,
+        on: $('lcd_text_color').value
+    });
 
     lcd0.font(0, [0, 10, 21, 17, 10, 4]);//save heart symbol at 0 index
     lcd0.char(0, 0, String.fromCharCode(0));//draw custom symbol saved at 0 index
@@ -58,6 +65,34 @@ var initFullViews = () => {
     }
 }
 
+var initPanels = () => {
+
+}
+
+var elValues = [
+    { name: 'full_view_cp', defvalue: 'eu' },
+    { name: 'rows', defvalue: 2 },
+    { name: 'columns', defvalue: 16 },
+    { name: 'px_size', defvalue: 3 },
+    { name: 'break_size', defvalue: 1 },
+    { name: 'lcd_bg_color', defvalue: '#cd2' },
+    { name: 'lcd_text_color', defvalue: '#143' },
+];
+
+var saveState = () => {
+    elValues.forEach(element => {
+        localStorage.setItem('LCDtest_' + element.name, $(element.name).value);
+    });
+}
+
+var loadState = () => {
+    elValues.forEach(element => {
+        $v(element.name, localStorage.getItem('LCDtest_' + element.name) || element.defvalue)
+    });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
+    loadState();
     initFullViews();
+    selMenu("tests");
 });
