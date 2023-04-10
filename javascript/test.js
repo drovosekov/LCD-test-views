b32624cd77e035e9999eb32ab69c545cd2be6f5a
Alexander Drovosekov <drovosekov@users.noreply.github.com>
2023-04-10 10:34:01 +0300

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -680,6 +680,11 @@ var dec2hex = (dec) => {
     return dec.toString(16);
 }
 
+var hex2dec = (hex) =>{
+    if(hex. length % 2) hex='0'+hex;
+    return BigInt('0x'+hex).to string(10);
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     loadState();
     initCustomSymbolsPanel();
